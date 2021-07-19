c60dcc706498aadf1eb4ad6cb4907ab6f79a1e21
wangkai0987 <52160353+wangkai0987@users.noreply.github.com>
2021-07-19 22:26:42 +0800

    mac 版本

--- main.c
+++ main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
+void getName() {
+
+}
+
+
 int main() {
+    getName();
     printf("Hello, World!\n");
     return 0;
 }
+
---
+++ test.h
@@ -0,0 +1,8 @@
+//
+// Created by 王凯 on 2021/7/19.
+//
+
+#ifndef UNTITLED_TEST_H
+#define UNTITLED_TEST_H
+
+#endif //UNTITLED_TEST_H
