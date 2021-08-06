9d44ed5c45f4276f93e261a1124f37da9f50548b
王凯 <wangk@szlanyou.com>
2021-08-06 09:03:25 +0800

    工程变动

--- wangkai/wkSample.c
+++ wangkai/wkSample.c
@@ -6,30 +6,9 @@
 
 void calculatePower() {
 
-    int num_1[100] = {
-            0, 1, 2, 3, 4,
-    };
-
-    int num_2 = num_1[4];
-
-    int b = 1 < -1;
-
-    /*while (1) {
-        printf("123");
-    }*/
-
     int num[MAXI] = {
-            1,2,3,4,5,6,7,8
+            0
     };
-
-    int* nResult0 = &num[0];
-    int* nResult1 = &num[1];
-    int* nResult2 = &num[2];
-
-    int nResult = *nResult0;
-
-    Frmt(&num[MAXI]);
-    Frmt(&num[MAXI]);
 
     num[0] = 7;
     for (int i = 0; i < POW - 1; i++) {
@@ -37,16 +16,15 @@ void calculatePower() {
         while (1) {
             if (num[j] == 0) {
                 break;
-            }
-            else {
+            } else {
                 num[j] *= 7;
                 j++;
             }
         }
         for (int ji = 0; ji < MAXI; ji++) {
-            if (num[ji] == 0)
+            if (num[ji] == 0) {
                 break;
-            else {
+            } else {
                 Frmt(&num[ji]);
             }
         }
